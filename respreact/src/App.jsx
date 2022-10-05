8639507be97cd440d9342c3445f3cda93a5bb0be
Ramsyz <seetaram.vannemreddi@gmail.com>
2022-10-05 07:40:37 -0400

    commit

---
+++ respreact/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Home from "./component/pages/Home";
+import Navbar from "./component/inc/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ respreact/src/component/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Slider from "./inc/Slider";
+
+export default function Home() {
+	return (
+		<>
+			<Slider />
+			<div className="container">
+				<div className="py-4 text-center"></div>
+				<h1>welcome</h1>
+			</div>
+		</>
+	);
+}
